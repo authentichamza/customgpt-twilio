9fcd822cd6ceab4e7dd2047e2db416acb6cb3520
authentichamza <hamzashams446@gmail.com>
2023-11-10 04:14:04 +0500

    make twilio respond once using celery background to hadle 15 sec timeout

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, Response
+from flask import Flask, request, make_response
 from twilio.twiml.messaging_response import MessagingResponse
 from dotenv import load_dotenv
 import os
@@ -14,17 +14,12 @@ app = Flask(__name__)
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    print(request.values)
     incoming_msg = request.values.get('Body', '').lower()
     message_from = request.values.get('From', '')
     process_sms.delay(message_from, incoming_msg)
-    response_msg = "Typing"
-    
-    resp = MessagingResponse()
-    resp.message(response_msg)
+    response_msg = make_response("OK", 200)
 
-    return Response(str(resp), mimetype="application/xml")     
- 
+    return response_msg
 
 if __name__ == '__main__':
     app.run(debug=True)
